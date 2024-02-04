7485ad235c5e0d711f0106eaabce8e130d57a36c
Artur <magisterbrownie@gmail.com>
2024-02-04 14:07:03 +0100

    viz working

--- app/src/routes/Matches.svelte
+++ app/src/routes/Matches.svelte
@@ -1,6 +1,7 @@
 <script>
     import {onMount} from 'svelte';
     import { authenticatedFetch } from '$lib/request.js'
+    import { backend_url } from '$lib/config.js'
     import Fa from 'svelte-fa/src/fa.svelte'
     import { faPlay } from '@fortawesome/free-solid-svg-icons'
     let matches = [];
@@ -16,14 +17,88 @@
         showAgent=undefined
         gameId=undefined
         matches=[]
+        clearInterval(intervalId);
     }
-    $: console.log(owner);
+    let intervalId; 
+   $: if(gameId !== undefined){
+        authenticatedFetch("/api/matches/recording?id="+gameId).then(resp => {
+            resp.json().then(body => {
+                let cst = 0;
+                intervalId = setInterval(function() {
+                    let state = body[cst%body.length];
+                    cst++;
+                    render(state)
+                    //Module.ccall("c_add", "number", ["string"],[state])
+                }, 1000);
+
+            })
+        });
+    }
+    function line(ctx, x0, y0, x1, y1){
+        ctx.beginPath();
+        ctx.moveTo(x0, y0);
+        ctx.lineTo(x1, y1);
+        ctx.stroke(); 
+     
+    }
+    function render(board_state) {
+        const canvas = document.getElementsByClassName("video")[0];
+        const ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        const offset = 40;
+        const spacing = 80;
+        var posx = offset;
+        var posy = offset;
+        var fslash = false;
+        var toprow = true;
+        var leftcol = true;
+        var figures = [];
+        const board = board_state.state.split(' ')[0];
+        for (const spot of board) {
+            if(spot==='/'){
+                posx=offset;
+                posy+=spacing;
+                toprow=false;
+                leftcol=true;
+            }
+            else{
+                var cells = parseInt(spot);
+                if(isNaN(cells)){
+                    cells=1;
+                    figures.push({posx: posx, posy: posy, color: spot})
+                }
+                for(var i=0; i<cells; i++){
+                    if(!leftcol)
+                        line(ctx, posx, posy, posx-spacing, posy);
+                    if(!toprow)
+                        line(ctx, posx, posy, posx, posy-spacing);
+                    if(!leftcol && !toprow) {
+                        if(fslash)
+                            line(ctx, posx-spacing, posy, posx, posy-spacing);
+                        else
+                            line(ctx, posx, posy, posx-spacing, posy-spacing);
+                        }
+                    fslash = !fslash;
+                    posx+=spacing;
+                    leftcol=false;
+                }
+            }
+
+        }
+        for(const fig of figures) {
+            ctx.beginPath();
+            ctx.arc(fig.posx, fig.posy, spacing/4, 0, 2 * Math.PI);
+            ctx.fillStyle = fig.color === "W" ? "#c8c8c8" : "#000";
+            ctx.fill();
+        }
+    }
 </script>
 {#if showAgent !== undefined}
 <div class="background" on:click|stopPropagation={hide}>
     <div class="content" on:click|stopPropagation={()=>{}}>
         {#if gameId !== undefined}
-            <div class="video"></div>
+            <canvas class="video" height="400" width="800">
+            </canvas>
         {:else}
             <div class="tops">
                 <div class="header">
@@ -63,7 +138,6 @@
 }
 .video{
     height: 23rem;
-    background-color: #212121;
     margin: 1.5rem;
 }
 .background{
--- renderer/rt/emcc_muc.sh
+++
@@ -1,1 +0,0 @@
-emcc ./mux.c -o run -lavformat -lavcodec -lavutil -lswresample -lswscale -lm -lz -lx264 -lva -lva-drm -ldrm -lX11 -lva-x11 -lvdpau
